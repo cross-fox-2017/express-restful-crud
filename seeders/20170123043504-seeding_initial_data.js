f01752d954944d12013f2389b9cd498110cffe77
Syanmil <syanmil@yahoo.com>
2017-01-23 12:06:53 +0700

    creating model, seeding data

---
+++ seeders/20170123043504-seeding_initial_data.js
@@ -0,0 +1,33 @@
+'use strict';
+const faker = require('faker')
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('Users', [
+      {name: faker.name.firstName(), email: faker.internet.email(), createdAt: new Date(), updatedAt: new Date()},
+      {name: faker.name.firstName(), email: faker.internet.email(), createdAt: new Date(), updatedAt: new Date()},
+      {name: faker.name.firstName(), email: faker.internet.email(), createdAt: new Date(), updatedAt: new Date()},
+      {name: faker.name.firstName(), email: faker.internet.email(), createdAt: new Date(), updatedAt: new Date()},
+    ], {})
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+    return queryInterface.bulkDelete('Users', null, {})
+  }
+};
