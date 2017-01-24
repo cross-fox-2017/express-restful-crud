9830511124902190765d6c07bbf1c3d20e61bf94
Isumi & Eri Irawan <pair+isumizumi.mrei91@hacktiv8.com>
2017-01-24 22:34:39 +0700

    on progress

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,27 @@ var models = require('../models');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  models.Todo.findAll({raw:true}).then(function(todos){
-    console.log(todos);
-    res.render('index',{title:"Todo List", todoData:todos});
+  models.Todo.findAll({include: models.User}).then(function(todos){
+    models.User.findAll({raw:true}).then(function(data){
+      res.render('index',{title:"Todo List", todoData:todos, users:data});
+    })
   })
 });
+
+router.post('/add', function (req, res, next) {
+  models.Todo.create({
+    title: req.body.title,
+    is_complete: req.body.isComplete,
+    UserId: req.body.UserId
+  }).then(function(data){
+    res.redirect('/')
+  })
+});
+
+router.get('/getUpdate/:id', function(req, res, next) {
+  models.Todo.findById(req.params.id).then(function (todos) {
+    res.render("update", {todo: todos})
+  })
+});
+
 module.exports = router;
