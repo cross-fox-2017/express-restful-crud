d3578617fbc9777d8cdd6a8443591a3c90817762
Didit Suryadi & Achmad Kamil <pair+didietsuryadi.kahmadsyt@devbootcamp.com>
2017-01-25 22:32:24 +0700

    helper belum

---
+++ routes/index.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var router = express.Router();
+let models = require('../models')
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  models.Todo.findAll({include: models.User}).then(function (find) {
+    res.render('index', { result: find});
+  })
+});
+
+router.post('/update', function(req, res, next) {
+  models.Todo.findById(req.body.id).then(function (find) {
+    find.update({
+      id: req.body.id,
+      title: req.body.title,
+    }).then(function(){
+      res.redirect('/')
+    })
+  })
+});
+
+router.get('/edit/:id', function(req, res, next) {
+  models.Todo.findById(req.params.id).then(function (find) {
+    res.render("edit", {hasil: find})
+  })
+});
+
+router.get('/delete/:id', function(req, res, next) {
+  models.Todo.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  res.redirect('/')
+});
+
+router.post('/add', function(req, res, next) {
+  models.Todo.create({title: req.body.title, is_complete: req.body.complete, UserId: req.body.UserId, createdAt: new Date()}).then(function (data) {
+    res.redirect('/')
+  })
+});
+
+module.exports = router;
