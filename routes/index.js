9dff6ac1e0c6494dea6df87075cc7482b54a7db4
Didit Suryadi & Achmad Kamil <pair+didietsuryadi.kahmadsyt@devbootcamp.com>
2017-01-26 10:18:40 +0700

    finally

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,9 @@ let models = require('../models')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   models.Todo.findAll({include: models.User}).then(function (find) {
-    res.render('index', { result: find});
+    models.User.findAll({raw:true}).then(function(data){
+      res.render('index', { result: find, users: data});
+    })
   })
 });
 
